b3e6132416d10009aca1c0b562dfdf4e40edc980
Kim Geon-Young <gykim@globalkonet.com>
2026-03-04 10:32:03 +0900

    로그인 ui 구현 & webserver 단순 추가

--- GK_Antenna/LoadPage.xaml.cs
+++ GK_Antenna/LoadPage.xaml.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GK_Antenna
 {
-    /// <summary>
-    /// Page1.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class LoadPage : Page
     {
         public LoadPage()
         {
             InitializeComponent();
+            Loaded += LoadPage_Loaded;
+        }
+
+        private async void LoadPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 2초 대기 (로딩 화면 효과)
+            await Task.Delay(2000);
+
+            // Login 페이지로 이동
+            NavigationService?.Navigate(new Login());
         }
     }
 }
